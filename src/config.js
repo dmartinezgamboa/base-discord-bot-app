ebc9db223fa309c777cd12ff70a690f5ce57b5be
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2024-03-25 01:19:50 -0700

    Improvements to register cli and options
    - Support `--register` and `--guild-id` as options on `npm start`
    - Refactors for register cli
        - `registerSlashCommands` is now a util
    - Refactors for bot config init

--- src/config.js
+++ src/config.js
@@ -4,13 +4,25 @@ const { GatewayIntentBits } = require("discord.js");
 const commands = require("./commands");
 const events = require("./events");
 
-const { TOKEN } = process.env;
+const {
+    npm_config_clear: CLEAR_REGISTERED_SLASH_COMMANDS,
+    npm_config_guild_id: GUILD_ID,
+    npm_config_register: REGISTER_SLASH_COMMANDS,
+    CLIENT_ID,
+    TOKEN,
+} = process.env;
+
+const intents = [GatewayIntentBits.Guilds];
 
 const CLIENT_CONFIGURATION = {
+    clear: CLEAR_REGISTERED_SLASH_COMMANDS,
+    clientID: CLIENT_ID,
+    guildID: GUILD_ID,
+    register: REGISTER_SLASH_COMMANDS,
     token: TOKEN,
     commands: commands,
     events: events,
-    intents: [GatewayIntentBits.Guilds],
+    intents: intents,
 };
 
 module.exports = { CLIENT_CONFIGURATION };
---
+++ src/register.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const commands = require("./commands");
+const { registerSlashCommands } = require("./utils/registerSlashCommands");
+
+const {
+    npm_config_clear: CLEAR_REGISTERED_SLASH_COMMANDS,
+    npm_config_guild_id: GUILD_ID,
+    CLIENT_ID,
+    TOKEN,
+} = process.env;
+
+const params = {
+    commands: commands,
+    clear: CLEAR_REGISTERED_SLASH_COMMANDS,
+    clientID: CLIENT_ID,
+    guildID: GUILD_ID,
+    token: TOKEN,
+};
+
+console.log(params);
+registerSlashCommands(params);
